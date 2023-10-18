2be53afae26e650a53a0c1d670d15cb9fe76bb72
rishabhJoshiKait <148195367+rishabhJoshiKait@users.noreply.github.com>
2023-10-18 16:32:55 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_Database='mysql+pymysql://root:rishabh123@localhost:3306/Fleetrez'
+URL_Database='postgresql://kai_api_user:eJix0ZnC4ohapciIDXVpmbJDI4bnLWK1@dpg-cknasoiv7m0s73bu4psg-a.singapore-postgres.render.com/kai_api'
 engine = create_engine(URL_Database)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
