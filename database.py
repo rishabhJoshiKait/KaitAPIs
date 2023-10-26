aae09ff829d91fc5e3ebf12b262cdc1a3c0a843a
rishabhJoshiKait <148195367+rishabhJoshiKait@users.noreply.github.com>
2023-10-26 20:09:40 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-import psycopg2
 
-URL_Database='postgresql://kai_api_ckgg_user:Y4h8sZYtz6kl9z73VR2t5dKQEngW1bx4@dpg-cksihupqhc9c73d3rdh0-a.singapore-postgres.render.com/kai_api_ckgg'
+URL_Database='postgresql://root:dPbtiKHQAltVPinKlHrx4tMYhcYlAc4t@dpg-ckt62jo168ec73efttj0-a.oregon-postgres.render.com/fleet_data'
 # URL_Database='mysql+pymysql://root:rishabh123@localhost:3306/fleetrez1'
 engine = create_engine(URL_Database)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+# postgres://root:dPbtiKHQAltVPinKlHrx4tMYhcYlAc4t@dpg-ckt62jo168ec73efttj0-a.oregon-postgres.render.com/fleet_data
